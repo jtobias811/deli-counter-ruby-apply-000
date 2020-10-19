acc783a1d779029c42044f17e4c545f358bb90cd
Jacob Tobias <jtobias811@gmail.com>
2020-10-19 16:53:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,5 +15,5 @@ def line(katz_deli)
     current_line << "#{index + 1}. #{name}"
     end
   end
-  puts "The line is currently: #{current_line.join(" ")}"
+  puts "The line is currently: #{current_line.join(" ")}."
 end
