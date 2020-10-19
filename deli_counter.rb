d96448801e26a57bb26ec107f54379aa1393d558
Jacob Tobias <jtobias811@gmail.com>
2020-10-19 16:39:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,4 @@
-# Write your code here.
+require 'pry'
 katz_deli = []
 
 def take_a_number(katz_deli, name)
@@ -12,6 +12,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     katz_deli.each_with_index do |name, index|
+      binding.pry
     current_line << "#{index + 1}. #{name}"
     end
   end
