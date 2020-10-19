b3025c04bdec2e97278d2cb687807773953220fd
Jacob Tobias <jtobias811@gmail.com>
2020-10-19 16:27:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,11 @@
 # Write your code here.
 katz_deli = []
 
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome #{name}, you are number #{katz_deli.length} in line."
+end
+
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
@@ -9,9 +14,4 @@ def line(katz_deli)
       puts "#{name} is number #{index + 1} in line. "
     end
   end
-end
-
-def take_a_number(katz_deli, name)
-  katz_deli << name
-  puts "Welcome #{name}, you are number #{katz_deli.length} in line."
 end
