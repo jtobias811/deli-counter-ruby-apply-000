7e3d922fa806fe897269bd810f81947864aeba61
Jacob Tobias <jtobias811@gmail.com>
2020-10-19 16:08:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,16 @@
 # Write your code here.
 katz_deli = []
 
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty"
+  else
+    katz_deli.each_with_index do |name, index|
+      puts "#{name} is number #{index} in line. "
+    end
+  end
+end
+
 def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome #{name}, you are number #{katz_deli.length} in line."
